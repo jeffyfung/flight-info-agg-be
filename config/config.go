e5f3a1647cc43bfc7fee01550034072d37c30ebd
Jeffrey Fung <jeffyfungkh@gmail.com>
2024-01-28 10:12:47 +0000

    Refactor: Load config for prod env

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -20,7 +21,6 @@ type Config struct {
 	Database struct {
 		MongodbUri string `required:"true" envconfig:"FLIGHTAGG_MONGODB_URI"`
 	}
-	Prod  bool `default:"false" envconfig:"FLIGHTAGG_PROD"`
 	Email struct {
 		SendGridAPIKey string `required:"true" envconfig:"FLIGHTAGG_SENDGRID_API_KEY"`
 		FromEmail      string `required:"true" envconfig:"FLIGHTAGG_FROM_EMAIL"`
@@ -30,6 +30,12 @@ type Config struct {
 var Cfg Config
 
 func LoadConfig() {
+	if os.Getenv("PROD") == "true" {
+		Cfg = loadConfigFromVariables()
+		fmt.Println("Cfg", Cfg.Email.FromEmail)
+		return
+	}
+
 	curDir, err := os.Getwd()
 	if err != nil {
 		log.Fatal("Cannot get cwd: ", err.Error())
@@ -45,4 +51,18 @@ func LoadConfig() {
 		log.Fatal("Cannot parse env variables: ", err.Error())
 	}
 	Cfg = cfg
+}
+
+func loadConfigFromVariables() Config {
+	cfg := Config{}
+	cfg.Server.Port = os.Getenv("FLIGHTAGG_PORT")
+	cfg.Server.Secret = []byte(os.Getenv("FLIGHTAGG_SECRET"))
+	cfg.Server.GoogleClientID = os.Getenv("FLIGHTAGG_GOOGLE_CLIENT_ID")
+	cfg.Server.GoogleClientSecret = os.Getenv("FLIGHTAGG_GOOGLE_CLIENT_SECRET")
+	cfg.Server.GithubClientID = os.Getenv("FLIGHTAGG_GITHUB_CLIENT_ID")
+	cfg.Server.GithubClientSecret = os.Getenv("FLIGHTAGG_GITHUB_CLIENT_SECRET")
+	cfg.Database.MongodbUri = os.Getenv("FLIGHTAGG_MONGODB_URI")
+	cfg.Email.SendGridAPIKey = os.Getenv("FLIGHTAGG_SENDGRID_API_KEY")
+	cfg.Email.FromEmail = os.Getenv("FLIGHTAGG_FROM_EMAIL")
+	return cfg
 }
